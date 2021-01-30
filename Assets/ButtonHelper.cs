0ea221b71e3fe70e098e47d048dca528e230139a
Mikael Hanssen <hanssen.mikael@gmail.com>
2021-01-30 16:41:47 +0100

    Add dialog system and scene navigation (WIP)

---
+++ Assets/ButtonHelper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonHelper : MonoBehaviour {
+	private DialogRenderer dialogRenderer;
+	private string activeScene = "Room1";
+
+	private void Start() {
+		dialogRenderer = FindObjectOfType<DialogRenderer>();
+	}
+
+	public void LoadScene(string name) {
+		Debug.Log("Unloading " + activeScene);
+		SceneManager.UnloadScene(activeScene);
+		activeScene = name;
+		DisplayMessage("");
+		Debug.Log("Loading " + name);
+		SceneManager.LoadScene(name, LoadSceneMode.Additive);
+	}
+
+	public void DisplayMessage(string message) {
+		dialogRenderer.Render(message);
+	}
+}
---
+++ Assets/Scripts/DialogRenderer.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogRenderer : MonoBehaviour {
+	public float writeSpeed = 50;
+
+	private Text output;
+	private float timer;
+	private string message = "";
+	private int charI;
+
+	private void Start() {
+		output = GetComponent<Text>();
+	}
+
+	public void Render(string message) {
+		charI = 0;
+		timer = Time.time;
+		output.text = "";
+	}
+
+	private void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			charI = message.Length;
+			output.text = message;
+		}
+
+		if (Time.time - timer >= 1f / writeSpeed && charI < message.Length) {
+			timer = Time.time;
+			output.text += message[charI];
+			charI++;
+		}
+	}
+}
