5dbeb8494efad9388408ab1676d67383c06fd3f0
avokadoen <akselhallajs@gmail.com>
2021-01-30 20:20:11 +0100

    cursor stuff

---
+++ Assets/Scripts/Pickupable.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Pickupable : MonoBehaviour
+{
+    CursorDisplay cd;
+
+    private void Start()
+    {
+        cd = GameObject.FindGameObjectWithTag("GameController").GetComponent<CursorDisplay>(); 
+    }
+
+    private void OnMouseEnter()
+    {
+        cd.SetGrabCursor();
+    }
+
+    private void OnMouseDown()
+    {
+        StartCoroutine(Held());
+    }
+
+    private void OnMouseUp()
+    {
+        StopCoroutine(Held());
+
+        // TODO: resolve outcome
+    }
+
+    private void OnMouseExit()
+    {
+        cd.SetDefaultCursor();
+    }
+
+    IEnumerator Held()
+    {
+
+        yield return new WaitForFixedUpdate();
+    }
+
+}
