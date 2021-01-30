cb998a9b5247db15d28f3f60de7f95f48838c3cd
avokadoen <akselhallajs@gmail.com>
2021-01-30 23:04:14 +0100

    Added hacky way of turning off player input

---
+++ Assets/Scripts/MouseOvertaker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseOvertaker : MonoBehaviour
+{
+    [SerializeField]
+    private PlayerMovement pm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!pm)
+            Debug.LogError($"Missing pm reference in {gameObject.name}!");
+    }
+
+    private void OnMouseOver()
+    {
+        pm.MouseActive = false;
+    }
+
+    private void OnMouseExit()
+    {
+        pm.MouseActive = true;
+    }
+}
