11f181bc31134aa3e727a9ef8b400a99f3b6ea93
Mikael Hanssen <hanssen.mikael@gmail.com>
2021-01-30 19:56:27 +0100

    Auto-load PERSISTENT scene on game start

---
+++ Assets/Scripts/Editor/DefaultSceneLoader.cs
@@ -0,0 +1,22 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+[InitializeOnLoad]
+/// Source: https://stackoverflow.com/questions/35586103/unity3d-load-a-specific-scene-on-play-mode
+public static class DefaultSceneLoader {
+	static DefaultSceneLoader() {
+		EditorApplication.playModeStateChanged += LoadDefaultScene;
+	}
+
+	static void LoadDefaultScene(PlayModeStateChange state) {
+		if (state == PlayModeStateChange.ExitingEditMode) {
+			EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo();
+		}
+
+		if (state == PlayModeStateChange.EnteredPlayMode) {
+			UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+		}
+	}
+}
+#endif
