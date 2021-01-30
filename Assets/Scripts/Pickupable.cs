2f81a238b596691685c2f694244c2d610b26f986
avokadoen <akselhallajs@gmail.com>
2021-01-30 21:04:43 +0100

    Ability to grab some objects

--- Assets/Scripts/Pickupable.cs
+++ Assets/Scripts/Pickupable.cs
@@ -2,32 +2,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-[RequireComponent(typeof(Collider2D))]
+[RequireComponent(typeof(Collider2D), typeof(SpriteRenderer))]
 public class Pickupable : MonoBehaviour
 {
-    CursorDisplay cd;
+    [SerializeField]
+    private int heldSortOrder = 11;
+    private int prevOrder = 0;
+
+    private SpriteRenderer sr;
+    private CursorDisplay cd;
+    private Vector3 prevPos;
+    private bool holding = false;
+
+    private void Awake()
+    {
+        cd = FindObjectOfType<CursorDisplay>();
+    }
 
     private void Start()
     {
-        cd = GameObject.FindGameObjectWithTag("GameController").GetComponent<CursorDisplay>(); 
+        sr = GetComponent<SpriteRenderer>();
     }
 
     private void OnMouseEnter()
     {
+        if (holding)
+            return;
+
         cd.SetGrabCursor();
+        prevPos = transform.position;
     }
 
     private void OnMouseDown()
     {
+        holding = true;
+        prevOrder = sr.sortingOrder;
+        sr.sortingOrder = heldSortOrder;
         StartCoroutine(Held());
     }
 
     private void OnMouseUp()
     {
-        StopCoroutine(Held());
-
-        // TODO: resolve outcome
+        holding = false;
+        sr.sortingOrder = prevOrder;
+        StopAllCoroutines();
+        StartCoroutine(ResolveDrop());
     }
 
     private void OnMouseExit()
@@ -37,8 +58,23 @@ public class Pickupable : MonoBehaviour
 
     IEnumerator Held()
     {
+        while(holding)
+        {
+            yield return new WaitForEndOfFrame();
+            var mousePos = Input.mousePosition;
+            Vector2 worldMousePos = Camera.main.ScreenToWorldPoint(mousePos);
+            transform.position = worldMousePos;
+        }
+    }
 
-        yield return new WaitForFixedUpdate();
+    IEnumerator ResolveDrop()
+    {
+        yield return new WaitForEndOfFrame();
+        // TODO: resolve outcome
+        // if (in inventory slot)
+        //      insert to inventory
+        // else
+        transform.position = prevPos;
     }
 
 }
