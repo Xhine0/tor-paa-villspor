185ce1e6c40b4f9a2889007a8b6d24f46d90a2b7
avokadoen <akselhallajs@gmail.com>
2021-01-31 16:36:30 +0100

    loke morph

---
+++ Assets/Scripts/LokeMorph.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class LokeMorph : MonoBehaviour
+{
+    Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        animator.SetBool("startMorph", true);
+    }
+}
