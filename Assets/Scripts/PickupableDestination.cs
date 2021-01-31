29bf59f18c63510ed979f3f0881623fffb29aaf7
avokadoen <akselhallajs@gmail.com>
2021-01-31 12:40:19 +0100

    Items can now interact with scene

--- Assets/Scripts/PickupableDestination.cs
+++ Assets/Scripts/PickupableDestination.cs
@@ -8,16 +8,40 @@ public class PickupableDestination : MonoBehaviour {
 	public string prefName;
 	public bool state;
 
-	public void Interact(GameObject item) {
-		if (item.name.Equals(itemName)) {
+    private HandInteractor hi;
+
+    private void Start()
+    {
+        hi = FindObjectOfType<HandInteractor>();
+    }
+
+    public bool Interact(Pickupable item) {
+        Debug.Log($"{itemName}, {item.name}");
+        if (item.name.Equals(itemName)) {
+            Debug.Log("hello");
 			PlayerPrefs.SetInt(prefName, state ? 1 : 0);
 			if (consumeItem) {
-				Destroy(item);
+				Destroy(item.gameObject);
 			}
 
 			foreach (EnableIf x in FindObjectsOfType<EnableIf>()) {
 				x.Check();
 			}
+            return true;
 		}
+
+        return false;
 	}
+
+    private void OnMouseEnter()
+    {
+        Debug.Log("heelo");
+        hi.RegisterDestination(this);
+    }
+
+    private void OnMouseExit()
+    {
+        Debug.Log("goodbye");
+        hi.UnRegisterDestination();
+    }
 }
