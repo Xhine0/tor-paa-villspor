8056e1a839d2dd9e842ab871b313ba5992e6e05a
avokadoen <akselhallajs@gmail.com>
2021-01-31 15:54:51 +0100

    Alvis stand when pick up hammer

---
+++ Assets/Scripts/WatchHammer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WatchHammer : MonoBehaviour
+{
+    [SerializeField]
+    Pickupable thorsHammer;
+
+    [SerializeField]
+    GameObject alvisStanding;
+
+    private bool firedEvent = false;
+
+    void Update()
+    {
+        if (!thorsHammer.gameObject.activeSelf && !firedEvent)
+        {
+            firedEvent = true;
+            alvisStanding.SetActive(true);
+            gameObject.SetActive(false);
+            PlayerPrefs.SetInt("hasHammer", 1);
+        }
+    }
+}
