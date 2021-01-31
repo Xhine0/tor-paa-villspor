62339408c5cf112a262e6330fb0d2c53363d2d68
avokadoen <akselhallajs@gmail.com>
2021-01-31 16:48:50 +0100

    reward wacking loke

---
+++ Assets/Scripts/WackLoke.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WackLoke : MonoBehaviour
+{
+    [SerializeField]
+    GameObject reward; 
+
+    PickupableDestination hammerDest;
+
+    private void Start()
+    {
+        hammerDest = GetComponent<PickupableDestination>();
+    }
+
+    private bool hasHappened = false;
+    void Update()
+    {
+        if (hasHappened)
+            return;
+
+        var wacked = PlayerPrefs.GetInt("wackLoke");
+        hasHappened = wacked == 1;
+        if (!hasHappened)
+            return;
+
+        Destroy(hammerDest);
+
+        reward.SetActive(true);
+    }
+}
